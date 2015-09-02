cf994928d11c98be65776cd45de1c6a4a2196adb
wewill147 <wewill147@gmail.com>
2015-09-02 12:36:41 -0500

    first commit

---
+++ models/FileList.php
@@ -0,0 +1,53 @@
+<?php namespace One47\FileList\Models;
+
+use Model;
+
+/**
+ * List Model
+ */
+class FileList extends Model
+{
+    use \October\Rain\Database\Traits\Validation;
+    
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'one47_filelist_filelists';
+
+    /**
+     * @var array Guarded fields
+     */
+    protected $guarded = ['*'];
+
+    /**
+     * @var array Validation Rules
+     */
+    public $rules = [
+      'name' => 'required|between:3,64',
+    ];
+
+    /**
+     * @var array Attach Many relation
+     */
+    public $attachMany = [
+      'files' => ['System\Models\File', 'order' => 'sort_order'],
+    ];
+
+    // /**
+    //  * @var array Fillable fields
+    //  */
+    // protected $fillable = [];
+
+    // /**
+    //  * @var array Relations
+    //  */
+    // public $hasOne = [];
+    // public $hasMany = [];
+    // public $belongsTo = [];
+    // public $belongsToMany = [];
+    // public $morphTo = [];
+    // public $morphOne = [];
+    // public $morphMany = [];
+    // public $attachOne = [];
+
+}
